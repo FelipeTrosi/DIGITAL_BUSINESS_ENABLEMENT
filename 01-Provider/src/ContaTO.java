03150b0ba2e9f4d00f8ed235a266c883eb1d6e8e
FelipeTrosi <38430386+FelipeTrosi@users.noreply.github.com>
2019-04-10 08:21:09 -0300

    Add files via upload

---
+++ 01-Provider/src/ContaTO.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+
+public class ContaTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private int codConta;
+	private Sting nome;
+	p
+
+}
